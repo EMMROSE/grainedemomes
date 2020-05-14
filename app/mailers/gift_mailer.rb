17294cb7d30b8e336f152fd53137be6f65a6aea5
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-05-14 10:34:38 +0200

    mailer

--- app/mailers/gift_mailer.rb
+++ app/mailers/gift_mailer.rb
@@ -5,36 +5,19 @@ class GiftMailer < ApplicationMailer
   #
   #   en.gift_mailer.confirmation.subject
   #
-  def confirmation
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
-
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.gift_mailer.information.subject
-  #
-  def information
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
-  end
-end
-
-def confirmation(order_gift)
+  def confirmation(order_gift)
     @order_gift = order_gift
     mail(
       to:       @order_gift.email,
       subject:  "Votre carte cadeau - Graîne de mômes"
     )
   end
-  def information(subscription)
-    @subscription = subscription
+  def information(order_gift)
+    @order_gift = order_gift
     mail(
-      to:       "emrose351@gmail.com",
+      to:       'contact@ecolibribox.com',
       subject:  "Nouvelle carte cadeau"
     )
   end
-contact@ecolibribox.com
+end
+
