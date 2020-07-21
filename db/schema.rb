2371ebe3d1b2525bc707dd95a0550fe470e9e54b
Emmanuel ROSE <roseemmanuel@hotmail.com>
2020-07-21 10:04:15 +0200

    promotion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_19_123150) do
+ActiveRecord::Schema.define(version: 2020_07_20_210852) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(version: 2020_07_19_123150) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "codepromo"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
